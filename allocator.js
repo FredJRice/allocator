d1d7a7a3acf14184e4f86266f7399adadb5f5497
FredJRice <fredjrice@outlook.com>
2024-10-03 21:33:22 +0100

    layout and add player to court mockup

--- allocator.js
+++ allocator.js
@@ -1,8 +1,14 @@
 'use strict';
 
-let player_pool =["John","Tim","Roger","Kevin","Sid","Fred","Arnold","Peter","Jim","Ken","Claire","Owen"];
+let player_pool =["John Black","Tim Oliver","Roger Keen","Kevin Otter","Sid Raymond","Fred Palmer","Arnold Butcher","Peter Kay","Jim Carrey","Ken Last","Claire Danes","Owen Wilson"];
 
 let current_player_pool =[];
 
+let court1 = document.querySelector('#one');
 
+court1.innerHTML = "Court 1" +"<br><br>"+"<strong>"+ player_pool[4]+"</strong><br><br>"+"V."+"<br><br><strong>"+player_pool[1]+"</strong>";
+
+// function addPlayer{
+
+// }
 
