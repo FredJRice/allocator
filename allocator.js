bdb930585b16402d947b19e8154162677df4ad4e
FredJRice <fredjrice@outlook.com>
2024-10-08 22:42:02 +0100

    Added player button and modal for pool players

--- allocator.js
+++ allocator.js
@@ -7,15 +7,34 @@ let player_pool =["Jack Black","Tim Allen","Roger Rabbit",
     "Bruce Willis", "Roger Moore", "Catherine Zeta-Jones",
     "Clive Owen", "Jack Nicholson", "Cate Blanchett"];
 
-let player_queue =[];
+addPlayer.addEventListener('click',addFromPoolToQueue)
+let pool = document.querySelector('#poolModal');
+let player_queue = []
+
+function addFromPoolToQueue(){
+    pool.style.display = "block";
+    player_queue = player_pool.pop()
+    console.log(player_queue)
+}
+
+// window.onclick = function(event) {
+//     if (event.target == poolModal) {
+//       pool.style.display = "none";
+//     }
+//   }
+
+
 
 let queue = document.querySelector('#playerQueue');
 
-player_pool.forEach(addPlayerToQueue);
 
-function addPlayerToQueue(item){
-    queue.innerHTML += "<div>"+"<br>" +item+"<br></div>";
-}
+
+// function addPlayerToQueue(){ 
+   
+//     // queue.innerHTML += "<div>"+"<br>" + returned_players +"<br></div>";
+   
+//     }
+
 
 
 
@@ -36,31 +55,31 @@ court2.addEventListener('click',addPlayersCourt2)
 court3.addEventListener('click',addPlayersCourt3)
 court4.addEventListener('click',addPlayersCourt4)
 
-function addPlayersCourt1(){
-    court1.innerHTML= "Court 1" +"<br><br>"+"<strong>"+ player_pool.pop()
-    +"</strong><br><br>"+"V."+"<br><br><strong>"+player_pool.pop()+"</strong>";
-    console.log(player_pool)
+function addPlayersCourt1(player_queue){
+    court1.innerHTML= "Court 1" +"<br><br>"+"<strong>"+ player_queue
+    +"</strong><br><br>"+"V."+"<br><br><strong>"+player_queue+"</strong>";
+    // console.log(player_pool)
     
 }
 
 function addPlayersCourt2(){
-    court2.innerHTML= "Court 2" +"<br><br>"+"<strong>"+ player_pool.pop()
-    +"</strong><br><br>"+"V."+"<br><br><strong>"+player_pool.pop()+"</strong>";
-    console.log(player_pool)
+    court2.innerHTML= "Court 2" +"<br><br>"+"<strong>"+ player_pool
+    +"</strong><br><br>"+"V."+"<br><br><strong>"+player_pool+"</strong>";
+    // console.log(player_pool)
     
 }
 
 function addPlayersCourt3(){
     court3.innerHTML= "Court 3" +"<br><br>"+"<strong>"+ player_pool.pop()
-    +"</strong><br><br>"+"V."+"<br><br><strong>"+player_pool.pop()+"</strong>";
-    console.log(player_pool)
+    +"</strong><br><br>"+"V."+"<br><br><strong>"+player_pool+"</strong>";
+    // console.log(player_pool)
     
 }
 
 function addPlayersCourt4(){
-    court4.innerHTML= "Court 4" +"<br><br>"+"<strong>"+ player_pool.pop()
-    +"</strong><br><br>"+"V."+"<br><br><strong>"+player_pool.pop()+"</strong>";
-    console.log(player_pool)
+    court4.innerHTML= "Court 4" +"<br><br>"+"<strong>"+ player_pool
+    +"</strong><br><br>"+"V."+"<br><br><strong>"+player_pool+"</strong>";
+    // console.log(player_pool)
 }
 
 
